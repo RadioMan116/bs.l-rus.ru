e9a59c5ca339902fcf4832b6f6909362683e1491
RadioMan116 <noris75564@gmail.com>
2020-11-16 14:39:51 +0300

    #контент По результатам проверки мобильной версии от 13.11.20 miele.ret-team.ru

--- templates/main/source/js/users/script.js
+++ templates/main/source/js/users/script.js
@@ -1981,6 +1981,25 @@ $(document).ready(function () {
 			return title;
 		},
 	});
+	tippy('.delivery__banner', {
+		// change these to your liking
+		arrow: true,
+		placement: 'top', // top, right, bottom, left
+		// trigger: 'click',
+		// maxWidth: 300, //px or string
+		interactive: true,
+		// leave these as they are
+		// followCursor: true,
+		allowHTML: true,
+		theme: 'light',
+		appendTo: () => document.body,
+		// ignoreAttributes: true,
+		content(reference) {
+			const title = reference.getAttribute('title');
+			reference.removeAttribute('title');
+			return title;
+		},
+	});
 	$('.product-card .link-pop-glossary').each(function (i, el) {
 		tippy(el, {
 			arrow: true,
